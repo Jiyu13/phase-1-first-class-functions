513d09823a7508369a827b0e8511daa4dee97ecb
Ziru Zou <jiyucoding@yahoo.com>
2023-01-09 20:49:02 -0500

    Create a function that returns a named function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function receivesAFunction(func) {
     func();
 }
+
+function returnsANamedFunction() {
+    const func = function() {
+        console.log("return a named function");
+    };
+
+    return func;
+}
